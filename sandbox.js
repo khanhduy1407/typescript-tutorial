862487c56e1f40f2336a476e606d768440ab033b
khanhduy1407 <kn145660@gmail.com>
2022-02-03 09:34:55 +0700

    Explicit Types

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,25 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = 'luigi';
-// age = "yoshi";
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(7.5));
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'luigi';
+age = 30;
+// isLoggedIn = 25;
+isLoggedIn = true;
+// arrays
+var ninjas = [];
+ninjas.push('shaun');
+// union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
+ninjaTwo = { name: 'mario', age: 20, beltColour: 'black' };
